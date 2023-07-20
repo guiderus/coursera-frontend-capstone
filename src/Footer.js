395a7b108eb8c876ee6b3aa8a31d5a2705bebda0
guiderus <guiderus@gmail.com>
2023-07-21 00:57:44 +0700

    Initial react app with simple component

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            Doormat navigation here
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+function Main() {
+    return (
+        <main>
+            <h2>This Week Specials!</h2>
+            <a href="/menu" role="button">Online Menu</a>
+            <div>To be Card component</div>
+            <div>To be Card component</div>
+            <div>To be Card component</div>
+        </main>
+    );
+}
+
+export default Main;
